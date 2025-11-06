dc90f47a1997cf83ee3114eface5a099ce142ff6
Hha <herman.adsten@gmail.com>
2025-11-06 20:35:00 +0100

    Added Pink Garnet and raw variation

--- src/main/java/com/hha/tutorialmod/TutorialMod.java
+++ src/main/java/com/hha/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.hha.tutorialmod;
 
+import com.hha.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/hha/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package com.hha.tutorialmod.block;
+
+import com.hha.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+    }
+}
