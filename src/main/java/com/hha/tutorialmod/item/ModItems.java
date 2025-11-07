f4118751614e5d63696e26604c294b5a948efc42
Hha <herman.adsten@gmail.com>
2025-11-07 17:26:04 +0100

    added magic thingies

--- src/main/java/com/hha/tutorialmod/item/ModItems.java
+++ src/main/java/com/hha/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.hha.tutorialmod.item;
 
 import com.hha.tutorialmod.TutorialMod;
+import com.hha.tutorialmod.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +12,8 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
